5e24e9cdfba61b438ccf3af52f6f23566def780a
Ciara Dees <ciara@founditmarketing.com>
2026-05-21 17:00:40 -0500

    Revert vercel.json rewrites to let Vercel handle Vite SPA routing automatically

--- generate-sitemap.js
+++ generate-sitemap.js
@@ -1,7 +1,7 @@
 import fs from 'fs';
 import path from 'path';
 
-const SITE_URL = 'https://brainsfoundationrepair.com';
+const SITE_URL = 'https://briansfoundationrepair.com';
 
 const routes = [
     '/',
